e30764e9a0735bf8bf72ce493cb4aa85674f27a3
klevain <klevain@protonmail.ch>
2021-04-27 16:20:39 +0200

    About content

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <b-container
+  fluid="xl"
+  class="about" >
+    <b-row
+      style="min-height: calc(100vh - 57px);"
+    >
+      <main role="main" class="col-12">
+        <!-- eslint-disable -->
+        <h1 class="mt-3">À propos de l'Abécédaire</h1>
+        <p>Ce projet à été créé pour deux raisons principales :  apprendre l'écriture cursive à ma fille et apprendre moi-même à utliser le stack technique suivant :</p>
+        <ul>
+          <li><a href="https://www.docker.com/">Docker</a></li>
+          <li><a href="https://nodejs.org/en/">Node</a></li>
+          <li><a href="https://cli.vuejs.org/">Vue CLI</a></li>
+          <li><a href="https://vuejs.org/">Vue</a></li>
+          <li><a href="https://getbootstrap.com/docs/4.6/">Boostrap</a></li>
+          <li><a href="https://bootstrap-vue.org/">Boostrap-Vue</a></li>
+          <li><a href="https://animejs.com/">AnimeJS</a></li>
+        </ul>
+        <p>Les couleurs de base ont été surclassées avec le nuancier "<a href="https://ethanschoonover.com/solarized/">Solarized</a>".</p>
+        <p>Les sources du projet et le guide d'installation sont sur <a href="https://github.com/klevain/abecedaire">GitHub</a>.</p>
+        <!-- eslint-enable -->
+      </main>
+    </b-row>
+  </b-container>
 </template>
